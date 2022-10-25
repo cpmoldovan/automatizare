4ec18fbd396ebb6649c029ac829a4503f8d891e6
selaru <cristian.saddler@gmail.com>
2022-10-25 11:13:36 +0300

    versiune care merge

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Creating a Simple HTTP Server using Python
 __This is the example for the project discussed in the blog post: [Simple HTTP Server using Python](https://codefires.com/creating-simple-http-server-python/)__
+
+
+# Cristi tips:
+1. foloseste fisierul .gitignore ca sa nu pui fisiere sensibile pe git din greseala https://git-scm.com/docs/gitignore
+2. 
--- web_server.py
+++ web_server.py
@@ -2,6 +2,8 @@ from distutils import text_file
 import sys
 import cgi
 import time
+import mimetypes
+import posixpath
 from docxtpl import DocxTemplate
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
@@ -25,65 +27,127 @@ def read_html_template(path):
 
 class PythonServer(SimpleHTTPRequestHandler):
     """Python HTTP Server that handles GET and POST requests"""
-    paths = ["/documente_create"]
-    flag = 1
+    paths = ["documente_create"]
+    flag = 0
     def do_GET(self):
+        path = self.translate_path(self.path)
         if self.path == '/':
             self.path = './templates/index.html'
             file = read_html_template(self.path)
             self.send_response(200, "OK")
             self.end_headers()
             self.wfile.write(bytes(file, "utf-8"))
+        #asta e pentru cazul cand URL-ul e catre un fisier si nu folder
+        elif not os.path.isdir(path):
+            ctype = self.guess_type(path)
+            try:
+                # Always read in binary mode. Opening files in text mode may cause
+                # newline translations, making the actual size of the content
+                # transmitted *less* than the content-length!
+                f = open(path, 'rb')
+                f.seek(0, os.SEEK_END)
+                filesize = f.tell()
+                f.seek(0, os.SEEK_SET)
+            except IOError:
+                self.send_error(404, "File not found")
+                return None
+            self.send_response(200)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(time.time()))
+            self.send_header("Cache-control", "no-cache, no-store, must-revalidate, max-age=0, proxy-revalidate, no-transform")
+            self.send_header("Pragma", "no-cache")
+            self.end_headers()
+        else:
+            path = './documente_create'
+            self.path = './templates/folder.html'
+            file = read_html_template(self.path)
+            table_row = ""
+            if self.flag == 0:
+                try:
+                    list = os.listdir(path)
+                except os.error:
+                    self.send_error(404, "No permission to list directory")
+                    return None
+            else:
+                try:
+                    list = self.paths
+                except os.error:
+                    self.send_error(404, "No permission to list directory")
+                    return None
+            list.sort(key=lambda a: a.lower())
 
-        if self.path == '/documente_create':
+            for dirname in list:
+                table_row += "<tr>"
+                name = os.path.basename(dirname)
+                name = name.encode("utf8")
+                fullname = dirname.encode('utf8')
+                displayname = linkname = name
+                # Append / for directories or @ for symbolic links
+                if os.path.isdir(fullname):
+                    displayname = name + "/"
+                    linkname = name + "/"
+                if os.path.islink(fullname):
+                    displayname = name + "@"
+                    # Note: a link to a directory displays with @ and links with /
+                table_row += "<td>"
+                table_row += '<a href="%s">%s</a>' % (urllib.parse.quote(fullname), urllib.parse.quote(fullname))
+                table_row += "</td>"
+                table_row += "</tr>"
+
+            file = file.replace("{{user_records}}", table_row)
             self.send_response(200, "OK")
             self.end_headers()
-            self.list_directory('documente_create')
+            self.wfile.write(bytes(file, "utf-8"))
 
-    def list_directory(self, path):
-        if self.flag == 0:
-            try:
-                list = os.listdir(path.decode("utf8"))
-            except os.error:
-                self.send_error(404, "No permission to list directory")
-                return None
+    def translate_path(self, path):
+        """Translate a /-separated PATH to the local filename syntax.
+        Components that mean special things to the local file system
+        (e.g. drive or directory names) are ignored.  (XXX They should
+        probably be diagnosed.)
+        """
+        # abandon query parameters
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = posixpath.normpath(urllib.parse.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        return path
+
+    def guess_type(self, path):
+        """Guess the type of a file.
+        Argument is a PATH (a filename).
+        Return value is a string of the form type/subtype,
+        usable for a MIME Content-type header.
+        The default implementation looks the file's extension
+        up in the table self.extensions_map, using application/octet-stream
+        as a default; however it would be permissible (if
+        slow) to look inside the data to make a better guess.
+        """
+
+        base, ext = posixpath.splitext(path)
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        ext = ext.lower()
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
         else:
-            try:
-                list = self.paths
-            except os.error:
-                self.send_error(404, "No permission to list directory")
-                return None
-        list.sort(key=lambda a: a.lower())
-        f = StringIO()
-        displaypath = "Requested Files"
-        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
-        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
-        f.write("<hr>\n<ul>\n")
-        for dirname in list:
-            name = os.path.basename(dirname)
-            name = name.encode("utf8")
-            fullname = dirname.encode('utf8')
-            displayname = linkname = name
-            # Append / for directories or @ for symbolic links
-            if os.path.isdir(fullname):
-                displayname = name + "/"
-                linkname = name + "/"
-            if os.path.islink(fullname):
-                displayname = name + "@"
-                # Note: a link to a directory displays with @ and links with /
-            
-            f.write('<li><a href="%s">%s</a>\n'
-                    % (urllib.parse.quote(fullname), urllib.parse.quote(fullname)))
-        f.write("</ul>\n<hr>\n</body>\n</html>\n")
-        length = f.tell()
-        f.seek(0)
-        self.send_response(200)
-        encoding = "utf-8"
-        self.send_header("Content-type", "text/html; charset=%s" % encoding)
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        return f
+            return self.extensions_map['']
+
+    if not mimetypes.inited:
+        mimetypes.init() # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream', # Default
+        '.docx': 'application/msword',
+        })
 
     def do_POST(self):
         if self.path == '/success':
